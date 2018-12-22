770d3d0f07267ea6d3e3205c36574d6aff603386
Owen Conti <owen@ohseemedia.com>
2018-12-22 11:42:32 -0700

    Initial commit

---
+++ RemoteAuthExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\RemoteAuth;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class RemoteAuthExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('remoteauth', __NAMESPACE__.'\Provider');
+    }
+}
